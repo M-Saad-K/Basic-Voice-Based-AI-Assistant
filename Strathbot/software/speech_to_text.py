2011dda853da0aa6ac0d3bdff1eabc78a36cbb60
M-Saad-K <mskscotland@gmail.com>
2026-07-21 11:04:07 +0100

    Swapping pyaudio for sounddevice

--- Strathbot/software/speech_to_text.py
+++ Strathbot/software/speech_to_text.py
@@ -1,28 +1,26 @@
 import vosk
-import pyaudio
+import sounddevice as sd
 import json
-import pygame
 
-pygame.mixer.init()
-model = vosk.Model("/home/mr-python/Documents/Summer Plan/Electronic Projects/Strathbot/packages/vosk-model-small-en-us-0.15")
+model = vosk.Model("/home/mr-python/Documents/Summer Plan/Electronic Projects/Basic-Voice-Based-AI-Assistant/Strathbot/packages/vosk-model-small-en-us-0.15")
 recognizer = vosk.KaldiRecognizer(model, 16000)
 
-## No playsound
-
 def listen_with_vosk():
-    mic = pyaudio.PyAudio()
-    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-    stream.start_stream()
-    print("Listening...")
-    ## Here we would play the sound
+    q = []
 
-    while True:
-        data = stream.read(4096, exception_on_overflow=False)
-        if len(data) == 0:
-            continue
-        if recognizer.AcceptWaveform(data):
-            ## Playsound here
-            result = recognizer.Result()
-            text = json.loads(result).get("text", "")
-            print("You said:", text)
-            return text
+    def callback(indata, frames, time, status):
+        q.append(bytes(indata))
+
+    with sd.RawInputStream(samplerate=16000, blocksize=8192, dtype="int16", channels=1, callback=callback):
+        print("Listening...")
+        while True:
+            if not q:
+                continue
+            data = q.pop(0)
+            if len(data) == 0:
+                continue
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                text = json.loads(result).get("text", "")
+                print("You said:", text)
+                return text
