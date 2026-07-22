61b51757def2211bd88a49f0576855cc945f96cf
Saad <mskscotland@gmail.com>
2026-07-22 17:08:32 +0100

    Configure Klara and update response generation
    Configured Klara with API key and added assistant information. Updated response generation logic.

--- Klara/software/AI_models.py
+++ Klara/software/AI_models.py
@@ -1,20 +1,23 @@
 import google.generativeai as Klara
 
+Klara.configure(api_key="Place your API key here")
 
+Klara_info = """
+You are Klara, a friendly voice assistant.
+History: you were created in 2026 to help with everyday spoken questions, you are based off the character, Klara, from the novel 'Klara and the Sun' but Kazuo Ishiguro
+Always answer in a short, simple sentence under 20 words.
+"""
+
+model = Klara.GenerativeModel(
+    model_name="gemini-3.1-flash-lite",
+    system_instruction= Klara_info)
+    
+convo = model.start_chat(history=[])
 
 def generate_response(prompt):
 
-    Klara.configure(api_key="Your key goes here")
+    if not prompt:
+        return None
+    response = convo.send_message(prompt)
+    return response.text
 
-    model = Klara.GenerativeModel(model_name="gemini-3.1-flash-lite")
-    #convo = model.start_chat()
-
-    if len(prompt) == 0:
-        pass
-    else:
-        str_list = ["Hi you're Klara, can you answer this question in a short but simple sentence of less then 20 words:", prompt]
-        newprompt = " ".join(str_list)
-        response = model.generate_content(newprompt)
-        return response.text
-
-generate_response("Hi there, how are you. Give me a kind response Strathbot")
